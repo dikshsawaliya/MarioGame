b22d412e4f0acba92e7f905fe25ad1d3dffc969f
dikshsawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2021-05-03 19:34:52 +0530

    Add files via upload

---
+++ Assets/Scripts/GameOver1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver1 : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        
+        SceneManager.LoadScene("SampleScene");
+    
+    }
+
+
+}
+
